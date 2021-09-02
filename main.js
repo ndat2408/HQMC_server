2d8a0baae9d31d428ed1287e08bbb4da0a453362
Nguyen The Dat <67305993+ndat2408@users.noreply.github.com>
2021-09-02 16:35:53 +0700

    final

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-document.getElementById("status").innerHTML = "Online";
-document.getElementById("status").style.color = "green";
-
 window.onscroll = function() {myFunction()};
 var navbar = document.querySelector(".header");
 var sticky = navbar.offsetTop;
@@ -9,11 +6,33 @@ function myFunction() {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
-    console.log(1)
   }
 }
 
-function copy(){
-    var copyText = document.getElementById("svname");
-    navigator.clipboard.writeText(copyText.innerText);
+var hour = new Date().getHours()
+if (hour >=8 && hour <12) {
+  document.getElementById('status').innerText = "(Server đang mở)";
+  document.getElementById('status').style.color = "green";
+} else {
+  document.getElementById('status').innerText = "(Server đã đóng)";
+  document.getElementById('status').style.color = "red";
+}
+
+document.querySelector(".modal").onclick = function(){
+  console.log(1)
+}
+
+function openmodal(){
+  document.querySelector(".modal").style.display = "block"
+}
+
+function closemodal(){
+  document.querySelector(".modal").style.display = "none"
+
+}
+
+function copy(e){
+  navigator.clipboard.writeText("THPTHQ.aternos.me");
+  e.classList.remove("fas")
+  e.classList.add("far")
 }
